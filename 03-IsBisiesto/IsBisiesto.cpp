371007abd976468b7b0303173f3504bb0312c429
Cristian O <63465945+cristianoreutn@users.noreply.github.com>
2020-06-09 21:08:46 -0300

    Update IsBisiesto.cpp

--- 03-IsBisiesto/IsBisiesto.cpp
+++ 03-IsBisiesto/IsBisiesto.cpp
@@ -14,8 +14,9 @@ bool IsBisiesto(int x);
 
 int main() {
     int x=1584;
-    assert(IsBisiesto(x)); //Prueba assert para comprobar que la función sea verdadera
-
+    
+     assert(IsBisiesto(x)); //Prueba assert para comprobar que la función sea verdadera
+    
      assert(IsBisiesto(1583)); //Prueba assert para comprobar que sea mayor o igual a 1584, primer año bisiesto
      assert(IsBisiesto(2019)); //Prueba assert para comprobar que sea divisible por 4
      assert(IsBisiesto(1900)); //Prueba assert para comprobar que es multiplo de 100 y no es divisible por 400
@@ -27,7 +28,7 @@ int main() {
 
 bool IsBisiesto(int x){
 
-        return ((x>=1584) and (x%4==0)) and ((x%100!=0)or (x%400==0));
+        return ((x>=1584) and (x%4==0)) and ((x%100!=0) or (x%400==0));
 
 }
 
